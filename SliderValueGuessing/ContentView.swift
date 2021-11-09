76ec4a9a54556f1f4b446a69cd1d74614c8edc1e
Nadzeya Shpakouskaya <shpakovna@gmail.com>
2021-11-09 09:52:19 +0400

    Create base UI Restart game function implementation

--- SliderValueGuessing/ContentView.swift
+++ SliderValueGuessing/ContentView.swift
@@ -8,9 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var sliderValue: Double = 50
+    @State private var guessValue: Int = 90
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            HStack{
+                Text("Move slider as close as you can to")
+                Text("\(guessValue)")
+                    .frame(width: 25)
+            }.font(.callout)
+               
+            HStack{
+                Text("0")
+                Slider(value: $sliderValue)
+                Text("100")
+            }.padding()
+            VStack(spacing: 20){
+                Button(action: checkValue) {
+                    Text("Check my guessing")
+                }
+                Button(action: restartGame) {
+                    Text("Restart game")
+                }
+            }
+            
+        }.padding()
+    }
+    
+    private func checkValue() {
+        
+    }
+    
+    private func restartGame() {
+        guessValue = Int.random(in: 0...100)
+        
     }
 }
 
