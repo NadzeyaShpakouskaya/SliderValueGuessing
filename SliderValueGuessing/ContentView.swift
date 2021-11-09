138b2e8bb424ee01543dff9d44208f8fe39decfe
Nadzeya Shpakouskaya <shpakovna@gmail.com>
2021-11-09 10:33:23 +0400

    Implementing logic of game

--- SliderValueGuessing/ContentView.swift
+++ SliderValueGuessing/ContentView.swift
@@ -9,25 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var sliderValue: Double = 50
-    @State private var guessValue: Int = 90
+    @State private var currentValue: Double = 50
+    @State private var targetValue: Int = 90
+
+
+    
+    @State private var isAlertPresenting =  false
     
     var body: some View {
         VStack{
             HStack{
                 Text("Move slider as close as you can to")
-                Text("\(guessValue)")
+                Text("\(targetValue)")
                     .frame(width: 25)
             }.font(.callout)
                
-            HStack{
-                Text("0")
-                Slider(value: $sliderValue)
-                Text("100")
-            }.padding()
+            SliderView(currentValue: $currentValue)
             VStack(spacing: 20){
                 Button(action: checkValue) {
                     Text("Check my guessing")
+                }
+                .alert(Text("Your score is \(calculateScore()) of 100"), isPresented: $isAlertPresenting) {
                 }
                 Button(action: restartGame) {
                     Text("Restart game")
@@ -38,12 +40,17 @@ struct ContentView: View {
     }
     
     private func checkValue() {
-        
+        isAlertPresenting.toggle()
     }
     
     private func restartGame() {
-        guessValue = Int.random(in: 0...100)
+        targetValue = Int.random(in: 0...100)
         
+    }
+    
+    private func calculateScore() -> Int {
+        let difference  = abs(targetValue - lround(currentValue))
+        return 100 - difference
     }
 }
 
@@ -51,4 +58,18 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+struct SliderView: View {
+    @Binding var currentValue: Double
+    
+    var body: some View {
+        HStack{
+            Text("0")
+            Slider(value: $currentValue, in: 0...100) {
+            }
+            Text("100")
+        }.padding()
+    }
+    
 }
