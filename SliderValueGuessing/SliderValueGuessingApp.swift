8ac4a0352fa46b139d6f0b400b35a246ea815883
Nadzeya Shpakouskaya <shpakovna@gmail.com>
2021-11-09 09:08:35 +0400

    Initial Commit

---
+++ SliderValueGuessing/SliderValueGuessingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderValueGuessingApp.swift
+//  SliderValueGuessing
+//
+//  Created by Nadzeya Shpakouskaya on 09/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct SliderValueGuessingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
